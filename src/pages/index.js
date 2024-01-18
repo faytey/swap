186fd7cd757513951daf403f36075827103a1ac4
Faith M. Roberts <faithroberts708@gmail.com>
2024-01-18 22:02:03 +0100

    fixes: hydration errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+"use client";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Layout from "../components/Layout";
 import { motion } from "framer-motion";
@@ -11,8 +12,16 @@ import {
   usePrepareContractWrite,
   useWaitForTransaction,
 } from "wagmi";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [oldTokenName, setOldTokenName] = useState("");
+  const [balanceOf, setBalanceOf] = useState("0");
+  const [symbol, setSymbol] = useState("");
+  const [newTokenName, setNewTokenName] = useState("");
+  const [streetBalanceOf, setStreetBalanceOf] = useState("0");
+  const [streetSymbol, setStreetSymbol] = useState("");
+  // const [oldTokenName, setOldTokenName] = useState("");
   const { address } = useAccount();
 
   const mainStContract = {
@@ -24,12 +33,6 @@ const Home = () => {
     address: "0xfD27B2De705Ee9d3FDb684BD69e1782D91BCbac3",
     abi: newAbi,
   };
-
-  const { config } = usePrepareContractWrite({
-    address: "0x39CE211F00b78b934279364a696ab6A6c812Bb78",
-    abi: converter,
-    functionName: "convert",
-  });
 
   const {
     data: readData,
@@ -73,21 +76,50 @@ const Home = () => {
     ],
   });
 
-  const balances = String(readData?.[1]?.result);
-  const streetBalances = String(readData?.[5]?.result);
+  useEffect(() => {
+    setOldTokenName(readData?.[0]?.result);
+    setBalanceOf(readData?.[1]?.result);
+    setSymbol(readData?.[2]?.result);
+    setNewTokenName(readData?.[4]?.result);
+    setStreetBalanceOf(readData?.[5]?.result);
+    setStreetSymbol(readData?.[6]?.result);
+  }, []);
+
+  const balances = String(balanceOf);
+  const streetBalances = String(streetBalanceOf);
 
   const balance = (Number(balances) / 1000000000).toLocaleString();
   const balanceTwo = (Number(streetBalances) / 1000000000).toLocaleString();
 
-  const {
-    data: writeContract,
-    isLoading: writeLoading,
-    write,
-  } = useContractWrite(config);
+  // const Write = async () => {
+  const { config } = usePrepareContractWrite({
+    ...mainStContract,
+    functionName: "approve",
+    args: ["0x39CE211F00b78b934279364a696ab6A6c812Bb78", Number(balanceOf)],
+  });
+
+  const { data, isLoading: writeLoad, write } = useContractWrite(config);
+
+  // const { config: convert } = usePrepareContractWrite({
+  //   address: "0x39CE211F00b78b934279364a696ab6A6c812Bb78",
+  //   abi: converter,
+  //   functionName: "convert",
+  // });
+
+  // const {
+  //   data: writeContract,
+  //   isLoading: writeLoading,
+  //   write: writeConvert,
+  // } = useContractWrite({
+  //   address: "0x39CE211F00b78b934279364a696ab6A6c812Bb78",
+  //   abi: converter,
+  //   functionName: "convert",
+  // });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     write?.();
+    // writeConvert?.();
   };
 
   const {
@@ -95,7 +127,7 @@ const Home = () => {
     isError: errorWaitData,
     isLoading: loadWaitData,
   } = useWaitForTransaction({
-    hash: writeContract?.hash,
+    hash: data?.hash,
 
     onError(error) {
       console.log("Error Message: ", error);
@@ -103,8 +135,26 @@ const Home = () => {
 
     onSuccess(data) {
       console.log("Success Message: ", data);
+      // writeConvert?.();
     },
   });
+
+  // const {
+  //   data: sendWait,
+  //   isError: errorWait,
+  //   isLoading: loadWait,
+  // } = useWaitForTransaction({
+  //   hash: writeConvert?.hash,
+
+  //   onError(error) {
+  //     console.log("Error Message: ", error);
+  //   },
+
+  //   onSuccess(data) {
+  //     console.log("Success Message: ", data);
+  //   },
+  // });
+  // };
 
   return (
     <Layout title="Token Swap">
@@ -135,22 +185,17 @@ const Home = () => {
           transition={{ delay: 0.6, duration: 0.6 }}
         >
           <div className="flex flex-col items-center space-y-4">
-            Your {readData?.[0].result} token balance is {balance ?? "0"}{" "}
-            {readData?.[2].result}
+            Your {oldTokenName} token balance is {balance} {symbol}
           </div>
           <div className="flex flex-col items-center space-y-4">
-            Your {readData?.[4].result} token balance is {balanceTwo ?? "0"}{" "}
-            {readData?.[6].result}
+            Your {newTokenName} token balance is {balanceTwo} {streetSymbol}
           </div>
           <div className="flex flex-col items-center space-y-4"></div>
           <motion.button
-            className="bg-gradient-to-r from-purple-500 to-purple-600 text-white px-8 py-4 rounded-full hover:bg-opacity-80 transition duration-300 ease-in-out"
-            whileHover={{ scale: 1.05 }}
+            className="bg-gradient-to-r from-purple-500 to-purple-600 text-white hover:cursor-pointer px-8 py-4 rounded-full hover:bg-opacity-80 transition duration-300 ease-in-out"
             onClick={handleSubmit}
           >
-            {writeContract || sendWaitData
-              ? "Converting"
-              : "Convert Your Tokens"}
+            {writeLoad || loadWaitData ? "Converting" : "Convert Your Tokens"}
           </motion.button>
         </motion.div>
       </div>
